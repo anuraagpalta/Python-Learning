d6a60fc96070da9c43c59d699d936613827a4d8d
Anuraag Palta <palta.anuraag@gmail.com>
2026-08-01 00:23:15 +0100

    Add user input to hello program

--- hello.py
+++ hello.py
@@ -2,3 +2,7 @@ name = "Anuraag"
 
 print(f"Hello, {name}!")
 print("Python, VS Code, Git and GitHub are ready.")
+
+topic = input("What are you learning today")
+
+print(f"Great! You are learning {topic} today.")
